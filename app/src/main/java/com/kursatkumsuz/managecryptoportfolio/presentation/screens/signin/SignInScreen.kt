dc2b0a308c92d669cb252d682621957c9fe532c1
DrHistory <kursatkumsuz@gmail.com>
2022-11-14 17:57:12 +0300

    add custom TextFields

--- app/src/main/java/com/kursatkumsuz/managecryptoportfolio/presentation/screens/signin/SignInScreen.kt
+++ app/src/main/java/com/kursatkumsuz/managecryptoportfolio/presentation/screens/signin/SignInScreen.kt
@@ -3,24 +3,20 @@ package com.kursatkumsuz.managecryptoportfolio.presentation.screens.signin
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import com.kursatkumsuz.managecryptoportfolio.R
 import com.kursatkumsuz.managecryptoportfolio.presentation.components.CustomCircularProgress
+import com.kursatkumsuz.managecryptoportfolio.presentation.components.CustomInputText
+import com.kursatkumsuz.managecryptoportfolio.presentation.components.CustomPasswordText
 import com.kursatkumsuz.managecryptoportfolio.presentation.components.Toast
 import com.kursatkumsuz.managecryptoportfolio.util.Response
 
@@ -37,15 +33,8 @@ fun SignInScreen(navController: NavHostController) {
         verticalArrangement = Arrangement.Center
     ) {
 
-        val emailState = remember { mutableStateOf("") }
-        val passwordState = remember { mutableStateOf("") }
-        val passwordVisibility = remember { mutableStateOf(false) }
-
-        val icon = if (passwordVisibility.value) {
-            painterResource(id = R.drawable.ic_visible)
-        } else {
-            painterResource(id = R.drawable.ic_invisible)
-        }
+        var emailState by remember { mutableStateOf("") }
+        var passwordState by remember { mutableStateOf("") }
 
         Text(text = "Welcome", fontSize = 28.sp, fontWeight = FontWeight.Bold, color = Color.White)
 
@@ -60,56 +49,11 @@ fun SignInScreen(navController: NavHostController) {
 
         Spacer(modifier = Modifier.height(60.dp))
 
-        TextField(
-            value = emailState.value,
-            onValueChange = { emailState.value = it },
-            label = { Text(text = "E-mail") },
-            singleLine = true,
-            modifier = Modifier
-                .width(320.dp)
-                .height(65.dp),
-            colors = TextFieldDefaults.textFieldColors(
-                backgroundColor = Color(0xFF252A34),
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
-                disabledIndicatorColor = Color.Transparent,
-                textColor = Color(0xFF95A6C5)
-            ),
-            shape = RoundedCornerShape(20.dp)
-        )
+        CustomInputText(labelText = "Email",backgroundColor = Color(0xFF252A34)) { emailState = it }
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        TextField(
-            value = passwordState.value,
-            onValueChange = { passwordState.value = it },
-            label = { Text(text = "Password") },
-            singleLine = true,
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-            trailingIcon = {
-                IconButton(onClick = { passwordVisibility.value = !passwordVisibility.value }) {
-                    Icon(
-                        painter = icon,
-                        contentDescription = "Visibility Icon",
-                        tint = Color(0xFF4E515B)
-                    )
-                }
-            },
-            visualTransformation = if (passwordVisibility.value)
-                VisualTransformation.None
-            else PasswordVisualTransformation(),
-            modifier = Modifier
-                .width(320.dp)
-                .height(65.dp),
-            colors = TextFieldDefaults.textFieldColors(
-                backgroundColor = Color(0xFF252A34),
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
-                disabledIndicatorColor = Color.Transparent,
-                textColor = Color(0xFF95A6C5)
-            ),
-            shape = RoundedCornerShape(20.dp)
-        )
+        CustomPasswordText{ passwordState = it }
 
         Spacer(modifier = Modifier.height(60.dp))
 
@@ -118,18 +62,18 @@ fun SignInScreen(navController: NavHostController) {
             colors = ButtonDefaults.buttonColors(Color(0xFF4453CA)),
             shape = RoundedCornerShape(20.dp),
             onClick = {
-                viewModel.signIn(emailState.value, passwordState.value)
+                viewModel.signIn(emailState, passwordState)
             }) {
             Text(text = "Sign In", color = Color.White)
             Spacer(modifier = Modifier.width(5.dp))
             CheckSignInState(viewModel, navController)
         }
 
-        Row (verticalAlignment = Alignment.CenterVertically) {
-            Text(text = "Don't you have an account?" , fontSize = 14.sp, color = Color.LightGray)
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(text = "Don't you have an account?", fontSize = 14.sp, color = Color.LightGray)
             Spacer(modifier = Modifier.width(2.dp))
             TextButton(onClick = { navController.navigate("signup_screen") }) {
-                Text(text = "Sign Up" , fontSize = 14.sp, color = Color.Blue)
+                Text(text = "Sign Up", fontSize = 14.sp, color = Color.Blue)
 
             }
         }
@@ -141,7 +85,7 @@ fun SignInScreen(navController: NavHostController) {
 fun CheckSignInState(viewModel: SignInViewModel, navController: NavHostController) {
     val context = LocalContext.current
     val isLoading = remember { mutableStateOf(false) }
-    if(isLoading.value) CustomCircularProgress()
+    if (isLoading.value) CustomCircularProgress()
     LaunchedEffect(key1 = Unit) {
 
         viewModel.signInFlow.collect {
--- app/src/main/java/com/kursatkumsuz/managecryptoportfolio/presentation/screens/signup/SignUpScreen.kt
+++ app/src/main/java/com/kursatkumsuz/managecryptoportfolio/presentation/screens/signup/SignUpScreen.kt
@@ -3,27 +3,20 @@ package com.kursatkumsuz.managecryptoportfolio.presentation.screens.signup
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import com.kursatkumsuz.managecryptoportfolio.R
 import com.kursatkumsuz.managecryptoportfolio.presentation.components.CustomCircularProgress
+import com.kursatkumsuz.managecryptoportfolio.presentation.components.CustomInputText
+import com.kursatkumsuz.managecryptoportfolio.presentation.components.CustomPasswordText
 import com.kursatkumsuz.managecryptoportfolio.presentation.components.Toast
 import com.kursatkumsuz.managecryptoportfolio.util.Response
 
@@ -41,15 +34,8 @@ fun SignUpScreen(navController: NavHostController) {
         verticalArrangement = Arrangement.Center
     ) {
 
-        val emailState = remember { mutableStateOf("") }
-        val passwordState = remember { mutableStateOf("") }
-        val passwordVisibility = remember { mutableStateOf(false) }
-
-        val icon = if (passwordVisibility.value) {
-            painterResource(id = R.drawable.ic_visible)
-        } else {
-            painterResource(id = R.drawable.ic_invisible)
-        }
+        var emailState by remember { mutableStateOf("") }
+        var passwordState by remember { mutableStateOf("") }
 
         Text(
             text = "Create an account",
@@ -69,56 +55,11 @@ fun SignUpScreen(navController: NavHostController) {
 
         Spacer(modifier = Modifier.height(60.dp))
 
-        TextField(
-            value = emailState.value,
-            onValueChange = { emailState.value = it },
-            label = { Text(text = "E-mail") },
-            singleLine = true,
-            modifier = Modifier
-                .width(320.dp)
-                .height(65.dp),
-            colors = TextFieldDefaults.textFieldColors(
-                backgroundColor = Color(0xFF252A34),
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
-                disabledIndicatorColor = Color.Transparent,
-                textColor = Color(0xFF95A6C5)
-            ),
-            shape = RoundedCornerShape(20.dp)
-        )
+        CustomInputText(labelText = "Email",backgroundColor = Color(0xFF252A34)) { emailState = it }
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        TextField(
-            value = passwordState.value,
-            onValueChange = { passwordState.value = it },
-            label = { Text(text = "Password") },
-            singleLine = true,
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-            trailingIcon = {
-                IconButton(onClick = { passwordVisibility.value = !passwordVisibility.value }) {
-                    Icon(
-                        painter = icon,
-                        contentDescription = "Visibility Icon",
-                        tint = Color(0xFF4E515B)
-                    )
-                }
-            },
-            visualTransformation = if (passwordVisibility.value)
-                VisualTransformation.None
-            else PasswordVisualTransformation(),
-            modifier = Modifier
-                .width(320.dp)
-                .height(65.dp),
-            colors = TextFieldDefaults.textFieldColors(
-                backgroundColor = Color(0xFF252A34),
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
-                disabledIndicatorColor = Color.Transparent,
-                textColor = Color(0xFF95A6C5)
-            ),
-            shape = RoundedCornerShape(20.dp)
-        )
+        CustomPasswordText{ passwordState = it }
 
         Spacer(modifier = Modifier.height(60.dp))
 
@@ -127,7 +68,7 @@ fun SignUpScreen(navController: NavHostController) {
             colors = ButtonDefaults.buttonColors(Color(0xFF4453CA)),
             shape = RoundedCornerShape(20.dp),
             onClick = {
-                viewModel.signUp(emailState.value, passwordState.value)
+                viewModel.signUp(emailState, passwordState)
             }) {
             Text(text = "Create", color = Color.White)
             Spacer(modifier = Modifier.width(5.dp))
