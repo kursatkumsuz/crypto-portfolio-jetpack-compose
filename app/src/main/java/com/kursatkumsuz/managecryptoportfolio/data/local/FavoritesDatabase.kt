a9d7c0ae3ca08b4be121a1b75f5a9fc60c0742ad
DrHistory <kursatkumsuz@gmail.com>
2022-12-01 22:35:16 +0300

    add automatic migration

--- app/src/main/java/com/kursatkumsuz/managecryptoportfolio/data/local/FavoritesDatabase.kt
+++ app/src/main/java/com/kursatkumsuz/managecryptoportfolio/data/local/FavoritesDatabase.kt
@@ -1,11 +1,26 @@
 package com.kursatkumsuz.managecryptoportfolio.data.local
 
+import androidx.room.AutoMigration
 import androidx.room.Database
+import androidx.room.DeleteColumn
 import androidx.room.RoomDatabase
+import androidx.room.migration.AutoMigrationSpec
 import com.kursatkumsuz.managecryptoportfolio.domain.model.favorites.FavoritesEntity
 
-@Database(entities = [FavoritesEntity::class], version = 2)
+@Database(
+    entities = [FavoritesEntity::class],
+    version = 2,
+    autoMigrations = [
+        AutoMigration(
+            from = 1, to = 2, spec = FavoritesDatabase.MyAutoMigration::class
+        )
+    ]
+
+)
 abstract class FavoritesDatabase : RoomDatabase() {
+
+    @DeleteColumn (tableName = "favorites",columnName = "name")
+    class MyAutoMigration : AutoMigrationSpec
 
     abstract fun getDao(): FavoritesDao
 
